a5f9c4c0991c017940e754909430e44efdcf504e
catur sukoco <catur.sukoco@gmail.com>
2022-03-24 20:44:54 +0800

    fundamental

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+This is python code for
+1. sequential code
+2.
+"""
+
+# Sequential code
+
+print('Ibu berkata,"Pergi ke toko"')
+print('Budi menjawab, "Baik bu, Saya akan membeli apa?" ')
+print('Ibu menjawab,"Belikan 1 botol susu"')
+print('Budi pergi ke toko')
+print('dan berbelanja')
+
+# Percabangan
+jumlah_botol_susu = 10
+
+if jumlah_botol_susu > 1:
+    print('Budi membeli 1 botol susu')
+else:
+    print('Budi tidak membeli susu')
