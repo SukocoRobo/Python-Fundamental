e1656e78f3fd660d6bf4ac2ffecb88e3365544e6
catur sukoco <catur.sukoco@gmail.com>
2022-03-24 21:54:57 +0800

    perulangan dengan for dan while

--- Fundamental_python_training_perulangan_dengan_while.py
+++ Fundamental_python_training_perulangan_dengan_while.py
@@ -3,14 +3,22 @@ This is python code for looping "Read book" dengan while
 """
 jumlah_buku = 10
 print('Perintah ibu,"Baca semua buku hingga paham"')
+total_jumlah_baca = 0
 
-jumlah_buku_dibaca = 0
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}')
+jumlah_buku_dibaca_dan_dipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlah_buku_dibaca_dan_dipahami}')
 
-while jumlah_buku_dibaca < jumlah_buku:
-    print('Baca 1 buku yang belum dibaca')
-    jumlah_buku_dibaca = jumlah_buku_dibaca + 1
-    print(f"buku ke {jumlah_buku_dibaca} sudah di baca")
+while total_jumlah_baca < jumlah_buku *2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_buku_dibaca_dan_dipahami == 9:
+        print(f"buku ke {jumlah_buku_dibaca_dan_dipahami} belum pahami")
+    else:
+        jumlah_buku_dibaca_dan_dipahami = jumlah_buku_dibaca_dan_dipahami + 1
+        print(f"buku ke {jumlah_buku_dibaca_dan_dipahami} sudah di baca dan dipahami")
 
-
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}')
+if jumlah_buku_dibaca_dan_dipahami == jumlah_buku:
+    print('Bu, Semua buku sudah dibaca dan di pahami')
+else:
+    print(f'tidak semua buku sudah di pahami.'
+          f' anak hanya bisa memahami {jumlah_buku_dibaca_dan_dipahami}')
+# print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_dibaca_dan_dipahami}')
