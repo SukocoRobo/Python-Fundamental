044929c0befa63c77819dd7731f3c2f31972d89a
catur sukoco <catur.sukoco@gmail.com>
2022-03-26 15:33:44 +0800

    data sintax dictionary

--- data_syntax_dictionary.py
+++ data_syntax_dictionary.py
@@ -1,11 +1,36 @@
 users = {
-    'id': 1,
-    'name': 'Leanne Graham',
-    'username': 'Bret',
-    'email': 'Sincere@april.biz'
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lon": "81.1496"
+        }
+    }
 }
 print(users)
-print(users['id'])
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"]["street"])
+print(users["address"]["geo"]["lat"])
+print(users["address"]["geo"]["lon"])
+
+print(users)
+print(type(users))
+
+print("\n ubah dict to json")
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result.json', 'w') as file:
+    json.dump(users, file)
